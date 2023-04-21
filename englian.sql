42ab0690be2a67c866c9a216efb52300c5f15df9
Márta Krisztián <martakrisztian97@gmail.com>
2023-04-22 01:46:24 +0200

    Frissítve englian.sql

--- englian.sql
+++ englian.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2023. Ápr 17. 00:42
+-- Létrehozás ideje: 2023. Ápr 22. 01:45
 -- Kiszolgáló verziója: 10.4.28-MariaDB
 -- PHP verzió: 8.2.4
 
