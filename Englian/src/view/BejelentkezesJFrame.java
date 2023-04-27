2d1149861404d849c8a18f117025990e115f6b11
Márta Krisztián <martakrisztian97@gmail.com>
2023-04-27 19:03:08 +0200

    u

--- Englian/src/view/BejelentkezesJFrame.java
+++ Englian/src/view/BejelentkezesJFrame.java
@@ -47,9 +47,11 @@ public class BejelentkezesJFrame extends javax.swing.JFrame {
         String felhasznalonev = felhasznalonevTextField.getText();
         String jelszo = new String(jelszoPasswordField.getPassword());
         if (felhasznalonev.isEmpty() || jelszo.isEmpty()) {
-            JOptionPane.showMessageDialog(null, "Felhasználónév és jelszó megadása kötelező!", "Sikertelen bejelentkezés", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Felhasználónév és jelszó megadása kötelező!", 
+                    "Sikertelen bejelentkezés", JOptionPane.ERROR_MESSAGE);
         } else if (!felhasznalonevek.contains(felhasznalonev)) {
-            JOptionPane.showMessageDialog(null, "Hibás felhasználónév!", "Sikertelen bejelentkezés", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Hibás felhasználónév!", 
+                    "Sikertelen bejelentkezés", JOptionPane.ERROR_MESSAGE);
         } else {
             beFelh = kapcsolat.felhasznaloAdatokLekerdez(felhasznalonev);
             if (Hash.MD5(jelszo).equals(beFelh.getJelszo())) {
@@ -58,7 +60,8 @@ public class BejelentkezesJFrame extends javax.swing.JFrame {
                 menu.setVisible(true);
                 this.setVisible(false);
             } else {
-                JOptionPane.showMessageDialog(null, "Hibás jelszó!", "Sikertelen bejelentkezés", JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(null, "Hibás jelszó!", 
+                        "Sikertelen bejelentkezés", JOptionPane.ERROR_MESSAGE);
             }
         }
     }
